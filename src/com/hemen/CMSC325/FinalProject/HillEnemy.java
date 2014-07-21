79f2eee65d9bf13d8b792fcc86c26b0c2e1f6362
Joshua Hemen <melombuki@gmail.com>
2014-07-21 08:59:31 -0500

    Added the shells for new enemy types. Slide, and Hill.

---
+++ src/com/hemen/CMSC325/FinalProject/HillEnemy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hemen.CMSC325.FinalProject;
+
+/**
+ *
+ * @author melombuki
+ */
+public class HillEnemy extends Enemy {
+    
+}
