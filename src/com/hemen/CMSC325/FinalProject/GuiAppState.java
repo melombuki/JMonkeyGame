cb1fd8b54e08e9c9dda818f8e0f16963c5c655ca
Joshua Hemen <melombuki@gmail.com>
2014-07-22 22:42:01 -0500

    Still trying to get rid of this pull.

--- src/com/hemen/CMSC325/FinalProject/GuiAppState.java
+++ src/com/hemen/CMSC325/FinalProject/GuiAppState.java
@@ -76,7 +76,7 @@ public class GuiAppState extends AbstractAppState implements ScreenController {
     this.timer        = this.app.getTimer();
 
     // Init the empty highscores String array
-    scores = new String[] {"", "", "", "", ""};
+    scores = new String[] {"1", "2", "3", "4", "5"};
     
     // Create and show the beginning gui display
     disp = new NiftyJmeDisplay(
@@ -421,11 +421,6 @@ public class GuiAppState extends AbstractAppState implements ScreenController {
         
         // Map all previous scores to initials
         String[] line;
-        //Testing
-        System.out.println(scores.length);
-        for(String s: scores)
-            System.out.println(s);
-        //Testing
         for(int i = 0; i < scores.length; i++) {
             if(scores[i].length() > 0) {
                 line = scores[i].split(":");
